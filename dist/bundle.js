26a3f57871ed8dfb95953192d2f7bb64e0be69e0
root <root@DESKTOP-K681M02>
2023-07-03 11:41:51 -0300

    Commit 2

--- dist/bundle.js
+++ dist/bundle.js
@@ -71,50 +71,53 @@ $53ffd25df6034fb9$var$$input_profession?.setAttribute("autocomplete", "off");
 $53ffd25df6034fb9$var$$input_birth?.setAttribute("autocomplete", "off");
 $53ffd25df6034fb9$var$$button?.addEventListener("click", async (event)=>{
     event.preventDefault();
-    const user_name = $53ffd25df6034fb9$var$$input_name ? $53ffd25df6034fb9$var$$input_name.value.toString() : "";
-    const user_profession = $53ffd25df6034fb9$var$$input_profession ? $53ffd25df6034fb9$var$$input_profession.value.toString() : "";
-    const user_birthdate = $53ffd25df6034fb9$var$$input_birth ? $53ffd25df6034fb9$var$$input_birth.value.toString() : "";
-    const data = [
-        user_name,
-        user_profession,
-        user_birthdate
-    ];
-    await (0, $f855b82269797e02$export$fac6b4861de7f1ca)(data, "http://localhost:3333");
-    const users = await (0, $f855b82269797e02$export$fe8726939961c446)(data[0], "http://localhost:3333");
-    users.forEach((user)=>{
-        const newRow = document.createElement("tr");
-        newRow.innerHTML = `
-            <td>${user.id}</td>
-            <td>${user.name}</td>
-            <td>${user.profession}</td>
-            <td>${user.birthdate}</td>
-            <td>
-                <button class="update">Editar</button>
-                <button class="delete">Excluir</button>
-            </td>
-        `;
-        $53ffd25df6034fb9$var$$table?.appendChild(newRow);
-        const $button_update = newRow.querySelector(".update");
-        const $button_delete = newRow.querySelector(".delete");
-        $button_update?.addEventListener("click", ()=>{
-            alert("Todas as edi\xe7\xf5es s\xe3o opcionais, ou seja, n\xe3o \xe9 preciso completar todos os campos para atualizar, basta deixar em branco.");
-            const name_updated = prompt("Novo Nome:");
-            const profession_updated = prompt("Nova Profiss\xe3o:");
-            const birth_updated = prompt("Nova Data de Nascimento:");
-            const updatedData = [
-                name_updated !== null && name_updated !== "" ? name_updated : user.name,
-                profession_updated !== null && profession_updated !== "" ? profession_updated : user.profession,
-                birth_updated !== null && birth_updated !== "" ? birth_updated : user.birthdate
-            ];
-            (0, $f855b82269797e02$export$dc0df4e8eb0ce957)(updatedData, user.id, "http://localhost:3333").then((updatedUser)=>{
-                $53ffd25df6034fb9$var$updateTableRow(newRow, updatedUser);
+    if (!isNaN(Number($53ffd25df6034fb9$var$$input_name.value)) || !isNaN(Number($53ffd25df6034fb9$var$$input_profession.value))) alert("[ERROR]: N\xe3o \xe9 poss\xedvel colocar n\xfameros no campo de Nome e Profiss\xe3o!");
+    else {
+        const user_name = $53ffd25df6034fb9$var$$input_name ? $53ffd25df6034fb9$var$$input_name.value.toString() : "";
+        const user_profession = $53ffd25df6034fb9$var$$input_profession ? $53ffd25df6034fb9$var$$input_profession.value.toString() : "";
+        const user_birthdate = $53ffd25df6034fb9$var$$input_birth ? $53ffd25df6034fb9$var$$input_birth.value.toString() : "";
+        const data = [
+            user_name,
+            user_profession,
+            user_birthdate
+        ];
+        await (0, $f855b82269797e02$export$fac6b4861de7f1ca)(data, "http://localhost:3333");
+        const users = await (0, $f855b82269797e02$export$fe8726939961c446)(data[0], "http://localhost:3333");
+        users.forEach((user)=>{
+            const newRow = document.createElement("tr");
+            newRow.innerHTML = `
+				<td>${user.id}</td>
+				<td>${user.name}</td>
+				<td>${user.profession}</td>
+				<td>${user.birthdate}</td>
+				<td>
+					<button class="update">Editar</button>
+					<button class="delete">Excluir</button>
+				</td>
+			`;
+            $53ffd25df6034fb9$var$$table?.appendChild(newRow);
+            const $button_update = newRow.querySelector(".update");
+            const $button_delete = newRow.querySelector(".delete");
+            $button_update?.addEventListener("click", ()=>{
+                alert("Todas as edi\xe7\xf5es s\xe3o opcionais, ou seja, n\xe3o \xe9 preciso completar todos os campos para atualizar, basta deixar em branco.");
+                const name_updated = prompt("Novo Nome:");
+                const profession_updated = prompt("Nova Profiss\xe3o:");
+                const birth_updated = prompt("Nova Data de Nascimento:");
+                const updatedData = [
+                    name_updated !== null && name_updated !== "" ? name_updated : user.name,
+                    profession_updated !== null && profession_updated !== "" ? profession_updated : user.profession,
+                    birth_updated !== null && birth_updated !== "" ? birth_updated : user.birthdate
+                ];
+                (0, $f855b82269797e02$export$dc0df4e8eb0ce957)(updatedData, user.id, "http://localhost:3333").then((updatedUser)=>{
+                    $53ffd25df6034fb9$var$updateTableRow(newRow, updatedUser);
+                });
+            });
+            $button_delete?.addEventListener("click", async ()=>{
+                await (0, $f855b82269797e02$export$38dab5a9fea42f39)(user.id, "http://localhost:3333");
+                newRow.remove();
             });
         });
-        $button_delete?.addEventListener("click", async ()=>{
-            await (0, $f855b82269797e02$export$38dab5a9fea42f39)(user.id, "http://localhost:3333");
-            newRow.remove();
-        });
-    });
+    }
 });
 function $53ffd25df6034fb9$var$updateTableRow(row, data) {
     const [idCell, nameCell, professionCell, birthdateCell] = row.cells;
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { create_, read_, update_, delete_ } from "./routes/users";
 
 const $button: HTMLButtonElement | null = document.querySelector("#button");
 const $table = document.querySelector("table");
-const $input_name: HTMLInputElement | null = document.querySelector("input#name");
-const $input_profession: HTMLInputElement | null = document.querySelector("input#profession");
+const $input_name: HTMLInputElement = document.querySelector("input#name");
+const $input_profession: HTMLInputElement = document.querySelector("input#profession");
 const $input_birth: HTMLInputElement | null = document.querySelector("input#birth");
 
 $input_name?.setAttribute("autocomplete", "off");
@@ -14,60 +14,64 @@ $input_birth?.setAttribute("autocomplete", "off");
 $button?.addEventListener("click", async (event) => {
 	event.preventDefault();
 
-	const user_name = $input_name ? $input_name.value.toString() : "";
-	const user_profession = $input_profession ? $input_profession.value.toString() : "";
-	const user_birthdate = $input_birth ? $input_birth.value.toString() : "";
-
-	const data: string[] = [
-		user_name,
-		user_profession,
-		user_birthdate
-	];
-
-	await create_(data, "http://localhost:3333");
-
-	const users = await read_(data[0], "http://localhost:3333");
-
-	users.forEach((user: { id: string; name: string; profession: string; birthdate: string; }) => {
-		const newRow = document.createElement("tr");
-		newRow.innerHTML = `
-            <td>${user.id}</td>
-            <td>${user.name}</td>
-            <td>${user.profession}</td>
-            <td>${user.birthdate}</td>
-            <td>
-                <button class="update">Editar</button>
-                <button class="delete">Excluir</button>
-            </td>
-        `;
-
-		$table?.appendChild(newRow);
-
-		const $button_update: HTMLButtonElement | null = newRow.querySelector(".update");
-		const $button_delete: HTMLButtonElement | null = newRow.querySelector(".delete");
-
-		$button_update?.addEventListener("click", () => {
-			alert("Todas as edições são opcionais, ou seja, não é preciso completar todos os campos para atualizar, basta deixar em branco.");
-			const name_updated = prompt("Novo Nome:");
-			const profession_updated = prompt("Nova Profissão:");
-			const birth_updated = prompt("Nova Data de Nascimento:");
-    
-			const updatedData = [
-				name_updated !== null && name_updated !== "" ? name_updated : user.name,
-				profession_updated !== null && profession_updated !== "" ? profession_updated : user.profession,
-				birth_updated !== null && birth_updated !== "" ? birth_updated : user.birthdate
-			];
-
-			update_(updatedData, user.id, "http://localhost:3333").then((updatedUser: any) => {
-				updateTableRow(newRow, updatedUser);
+	if (!isNaN(Number($input_name.value)) || !isNaN(Number($input_profession.value))) {
+		alert("[ERROR]: Não é possível colocar números no campo de Nome e Profissão!");
+	} else {
+		const user_name = $input_name ? $input_name.value.toString() : "";
+		const user_profession = $input_profession ? $input_profession.value.toString() : "";
+		const user_birthdate = $input_birth ? $input_birth.value.toString() : "";
+	
+		const data: string[] = [
+			user_name,
+			user_profession,
+			user_birthdate
+		];
+	
+		await create_(data, "http://localhost:3333");
+	
+		const users = await read_(data[0], "http://localhost:3333");
+	
+		users.forEach((user: { id: string; name: string; profession: string; birthdate: string; }) => {
+			const newRow = document.createElement("tr");
+			newRow.innerHTML = `
+				<td>${user.id}</td>
+				<td>${user.name}</td>
+				<td>${user.profession}</td>
+				<td>${user.birthdate}</td>
+				<td>
+					<button class="update">Editar</button>
+					<button class="delete">Excluir</button>
+				</td>
+			`;
+	
+			$table?.appendChild(newRow);
+	
+			const $button_update: HTMLButtonElement | null = newRow.querySelector(".update");
+			const $button_delete: HTMLButtonElement | null = newRow.querySelector(".delete");
+	
+			$button_update?.addEventListener("click", () => {
+				alert("Todas as edições são opcionais, ou seja, não é preciso completar todos os campos para atualizar, basta deixar em branco.");
+				const name_updated = prompt("Novo Nome:");
+				const profession_updated = prompt("Nova Profissão:");
+				const birth_updated = prompt("Nova Data de Nascimento:");
+		
+				const updatedData = [
+					name_updated !== null && name_updated !== "" ? name_updated : user.name,
+					profession_updated !== null && profession_updated !== "" ? profession_updated : user.profession,
+					birth_updated !== null && birth_updated !== "" ? birth_updated : user.birthdate
+				];
+	
+				update_(updatedData, user.id, "http://localhost:3333").then((updatedUser: any) => {
+					updateTableRow(newRow, updatedUser);
+				});
+			});
+	
+			$button_delete?.addEventListener("click", async () => {
+				await delete_(user.id, "http://localhost:3333");
+				newRow.remove();
 			});
 		});
-
-		$button_delete?.addEventListener("click", async () => {
-			await delete_(user.id, "http://localhost:3333");
-			newRow.remove();
-		});
-	});
+	}
 });
 
 function updateTableRow(row: HTMLTableRowElement, data: any) {
